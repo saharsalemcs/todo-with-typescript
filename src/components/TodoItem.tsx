7b4ea54087769c045197dc02681103de93a9ebcd
saharsalemcs <saharsalemcs@gmail.com>
2026-09-02 05:09:16 +0300

    fix: bug with focus in edit mode

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,17 @@
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import type { TodoItemProps } from "../model";
 import { MdDone } from "react-icons/md";
+import { useEffect, useRef, useState } from "react";
 
 export default function TodoItem({ todo, todos, setTodos }: TodoItemProps) {
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.text);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [editMode]);
+
   function handleDone(id: number) {
     setTodos(
       todos.map((todo) =>
@@ -13,16 +22,43 @@ export default function TodoItem({ todo, todos, setTodos }: TodoItemProps) {
   function handleDelete(id: number) {
     setTodos(todos.filter((todo) => todo.id !== id));
   }
-  return (
-    <form className="flex p-4 mt-3 sm:w-[40%] w-full rounded-md bg-paper">
-      <span
-        className={`flex-1 text-2xl focus:outline-none ${todo.isDone ? "line-through" : ""}`}
-      >
-        {todo.text}{" "}
-      </span>
+  function handleEdit(e: React.FormEvent, id: number) {
+    e.preventDefault();
 
-      <div className="flex items-center justify-between  gap-5 cursor-pointer text-xl">
-        <span>
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, text: editTodo } : todo,
+      ),
+    );
+    setEditMode(false);
+  }
+  return (
+    <form
+      onSubmit={(e) => handleEdit(e, todo.id)}
+      className="flex p-4 mt-3 sm:w-[40%] w-full rounded-md bg-paper"
+    >
+      {editMode ? (
+        <input
+          ref={inputRef}
+          type="text"
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="bg-white flex-1 text-2xl focus:outline-none"
+        />
+      ) : (
+        <span
+          className={`flex-1 text-2xl focus:outline-none ${todo.isDone ? "line-through" : ""}`}
+        >
+          {todo.text}
+        </span>
+      )}
+
+      <div className="flex items-center justify-between gap-5 cursor-pointer text-xl">
+        <span
+          onClick={() => {
+            if (!editMode && !todo.isDone) setEditMode((prev) => !prev);
+          }}
+        >
           <AiFillEdit />
         </span>
         <span onClick={() => handleDelete(todo.id)}>
