46f208813a03c03b21ebbc62f04ee9c32ba39950
saharsalemcs <saharsalemcs@gmail.com>
2026-09-02 03:48:11 +0300

    feat: useState in todo input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import InputField from "./components/InputField";
+import type { Todo } from "./model";
+
 export default function App() {
+  const [todo, setTodo] = useState<string>("");
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function handleAdd(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), text: todo, isDone: false }]);
+      setTodo("");
+    }
+  }
+  console.log(todos);
   return (
-    <div className="app">
-      <h1 className="heading">Taskify</h1>
+    <div className="h-screen bg-[#2f74c0] font-neucha flex items-center flex-col">
+      <h1 className="uppercase text-white my-8 text-5xl text-center z-1">
+        Taskify
+      </h1>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 }
---
+++ src/components/InputField.tsx
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+export default function InputField({ todo, setTodo, handleAdd }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <form
+      className="flex items-center relative w-[80%]"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        ref={inputRef}
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        className="w-full bg-white border-none outline-none rounded-full text-xl px-6 py-4  duration-150 shadow-sm focus:shadow-[0_0_10px_1000px_rgb(0,0,0,0.5)]"
+        placeholder="Add a task..."
+      />
+      <button className="absolute m-3 w-12 h-12 bg-[#2f74c0] text-white rounded-full text-lg transition-transform duration-150 right-0 cursor-pointer shadow-xl/20 hover:bg-[#388ae2] active:scale-80 active:shadow-[0_0_5px_black]">
+        Add
+      </button>
+    </form>
+  );
+}
