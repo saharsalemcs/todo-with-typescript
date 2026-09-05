af2ff64a2e4747c6388df7db340cc3fd5898540a
saharsalemcs <saharsalemcs@gmail.com>
2026-09-05 21:19:21 +0300

    refactor: enhance Zustand store integration with persistence

--- src/store/useTodoStore.ts
+++ src/store/useTodoStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import type { Todo } from "../model";
+import { persist } from "zustand/middleware";
 
 interface TodoState {
   todos: Todo[];
@@ -9,38 +10,43 @@ interface TodoState {
   deleteTodo: (id: number) => void;
 }
 
-export const useTodoStore = create<TodoState>((set) => ({
-  todos: [],
+export const useTodoStore = create<TodoState>()(
+  persist(
+    (set) => ({
+      todos: [],
 
-  addTodo: (text) =>
-    set((state) => {
-      const newTodos = [
-        ...state.todos,
-        { id: Date.now(), text, isDone: false },
-      ];
-      return { todos: newTodos };
+      addTodo: (text) =>
+        set((state) => {
+          const newTodos = [
+            ...state.todos,
+            { id: Date.now(), text, isDone: false },
+          ];
+          return { todos: newTodos };
+        }),
+
+      toggleTodo: (id) =>
+        set((state) => {
+          const newTodos = state.todos.map((todo) =>
+            todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
+          );
+          return { todos: newTodos };
+        }),
+
+      editTodo: (id, newText) =>
+        set((state) => {
+          const newTodos = state.todos.map((todo) =>
+            todo.id === id ? { ...todo, text: newText } : todo,
+          );
+          return { todos: newTodos };
+        }),
+
+      deleteTodo: (id) =>
+        set((state) => ({
+          todos: state.todos.filter((todo) => todo.id !== id),
+        })),
     }),
-
-  toggleTodo: (id) =>
-    set((state) => {
-      const newTodos = state.todos.map((todo) =>
-        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
-      );
-      return { todos: newTodos };
-    }),
-
-  editTodo: (id, newText) =>
-    set((state) => {
-      const newTodos = state.todos.map((todo) =>
-        todo.id === id ? { ...todo, text: newText } : todo,
-      );
-      return { todos: newTodos };
-    }),
-
-  deleteTodo: (id) =>
-    set((state) => ({
-      todos: state.todos.filter((todo) => todo.id !== id),
-    })),
-}));
+    { name: "todo-storage", partialize: (state) => ({ todos: state.todos }) },
+  ),
+);
 
 //! #### return new array , no mutate on the original array ####
